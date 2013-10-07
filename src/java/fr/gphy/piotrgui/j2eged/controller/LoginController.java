8de21b5067c46150f172aa25b1a43c6a9fc77043
gcornut <gcornut@gmail.com>
2013-10-07 11:51:35 +0200

    Login screen

---
+++ src/java/fr/gphy/piotrgui/j2eged/controller/LoginController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.gphy.piotrgui.j2eged.controller;
+
+/**
+ *
+ * @author guillaume
+ */
+public class LoginController {
+    
+}
