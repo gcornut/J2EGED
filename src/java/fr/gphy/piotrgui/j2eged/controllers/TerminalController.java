500ccfc83424fdc2d8a05358a45f0282bc30022e
gcornut <gcornut@gmail.com>
2013-10-10 16:23:50 +0200

    Command link test

--- src/java/fr/gphy/piotrgui/j2eged/controllers/TerminalController.java
+++ src/java/fr/gphy/piotrgui/j2eged/controllers/TerminalController.java
@@ -5,8 +5,7 @@
 package fr.gphy.piotrgui.j2eged.controllers;
 
 import java.io.Serializable;
-import java.util.Date;  
-import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 
 /**
@@ -18,17 +17,6 @@ import javax.inject.Named;
 public class TerminalController implements Serializable {  
   
     public String handleCommand(String command, String[] params) {  
-        /*if(command.equals("greet")) {  
-            if(params.length > 0)  
-                return "Hello " + params[0];  
-            else  
-                return "Hello Stranger";  
-            }  
-            else if(command.equals("date"))  
-                return new Date().toString();  
-            else  
-                return command + " not found";  
-        }*/
-        return command;
+        return "toto";
     }  
 }  
