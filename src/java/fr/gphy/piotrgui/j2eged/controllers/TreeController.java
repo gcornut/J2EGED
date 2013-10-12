0c8f9cb694a29db8e6b4861861e3ae3bf12b7fb3
Piotr <pierre.cressant@gmail.com>
2013-10-12 17:38:07 +0200

    Initialises treeController and helper

--- src/java/fr/gphy/piotrgui/j2eged/controllers/TreeController.java
+++ src/java/fr/gphy/piotrgui/j2eged/controllers/TreeController.java
@@ -1,37 +1,42 @@
-package fr.gphy.piotrgui.j2eged.controllers; 
-  
+package fr.gphy.piotrgui.j2eged.controllers;
+
+import fr.gphy.piotrgui.j2eged.helpers.TreeHelper;
+import fr.gphy.piotrgui.j2eged.model.Folder;
 import java.io.Serializable;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import org.primefaces.model.TreeNode;  
-import org.primefaces.model.DefaultTreeNode;  
+import org.primefaces.model.TreeNode;
+import org.primefaces.model.DefaultTreeNode;
 
 @Named("TreeController")
 @SessionScoped
-public class TreeController implements Serializable {  
-      
-    private TreeNode root;  
-  
-    public TreeController() {  
-        root = new DefaultTreeNode("Root", null);  
-        TreeNode node0 = new DefaultTreeNode("Node 0", root);  
-        TreeNode node1 = new DefaultTreeNode("Node 1", root);  
-        TreeNode node2 = new DefaultTreeNode("Node 2", root);  
-          
-        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);  
-        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);  
-          
-        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);  
-        TreeNode node11 = new DefaultTreeNode("Node 1.1", node1);  
-          
-        TreeNode node000 = new DefaultTreeNode("Node 0.0.0", node00);  
-        TreeNode node001 = new DefaultTreeNode("Node 0.0.1", node00);  
-        TreeNode node010 = new DefaultTreeNode("Node 0.1.0", node01);  
-          
-        TreeNode node100 = new DefaultTreeNode("Node 1.0.0", node10);  
-    }  
-  
-    public TreeNode getRoot() {  
-        return root;  
-    }  
-}  
+public class TreeController implements Serializable {
+
+    private TreeNode root;
+    private TreeHelper helper;
+
+    public TreeController() {
+        this.helper = new TreeHelper();
+
+
+        root = new DefaultTreeNode("Root", null);
+        
+        this.initTree(root, this.helper.getFolders(null));
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void initTree(TreeNode root, List<Folder> list) {
+        if (list == null) {
+            return;
+        }
+
+        for (Folder f : list) {
+            TreeNode node = new DefaultTreeNode(f.getName(), root);
+            this.initTree(node, this.helper.getFolders(f.getIdFolder()));
+        }
+    }
+}
